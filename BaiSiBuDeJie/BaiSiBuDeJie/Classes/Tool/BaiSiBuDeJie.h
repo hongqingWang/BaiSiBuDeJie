14a702a41f38f9eae25121d6347193d899c9fb0b
wanghongqing <272338444@qq.com>
2017-12-05 22:02:26 +0800

    增加 pch 文件
    增加 pch 文件

---
+++ BaiSiBuDeJie/BaiSiBuDeJie/Classes/Tool/BaiSiBuDeJie.h
@@ -0,0 +1,19 @@
+//
+//  BaiSiBuDeJie.h
+//  BaiSiBuDeJie
+//
+//  Created by Mac on 05/12/2017.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import "UIView+QQ.h"
+
+#ifdef DEBUG
+
+#define QQLog(...) NSLog(__VA_ARGS__)
+
+#else
+
+#define QQLog(...)
+
+#endif
