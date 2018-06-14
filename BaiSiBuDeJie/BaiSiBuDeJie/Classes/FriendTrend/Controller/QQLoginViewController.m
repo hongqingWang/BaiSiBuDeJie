d38b7e18c7122f0f37355a1548c720c420990e25
hongqingWang <272338444@qq.com>
2018-06-14 18:58:52 +0800

    登录界面导航条处理完毕
    登录界面导航条处理完毕

--- BaiSiBuDeJie/BaiSiBuDeJie/Classes/FriendTrend/Controller/QQLoginViewController.m
+++ BaiSiBuDeJie/BaiSiBuDeJie/Classes/FriendTrend/Controller/QQLoginViewController.m
@@ -19,19 +19,13 @@
     // Do any additional setup after loading the view from its nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Event Response
+- (IBAction)close:(UIButton *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)clickRegister:(UIButton *)sender {
+    sender.selected = !sender.selected;
 }
-*/
 
 @end
