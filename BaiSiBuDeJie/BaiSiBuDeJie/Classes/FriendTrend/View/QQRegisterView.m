13ca9243654b058f92f944722ed9f50a928668fa
hongqingWang <272338444@qq.com>
2018-06-15 09:04:42 +0800

    添加RegisterView
    添加RegisterView

---
+++ BaiSiBuDeJie/BaiSiBuDeJie/Classes/FriendTrend/View/QQRegisterView.m
@@ -0,0 +1,21 @@
+//
+//  QQRegisterView.m
+//  BaiSiBuDeJie
+//
+//  Created by 王红庆 on 2018/6/15.
+//  Copyright © 2018年 Mac. All rights reserved.
+//
+
+#import "QQRegisterView.h"
+
+@implementation QQRegisterView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
