f1327a83b7914cd29ee9a4fa3c0354421d88ab44
hongqingWang <272338444@qq.com>
2018-06-16 15:37:45 +0800

    自定义文本框
    自定义文本框，设置光标颜色

---
+++ BaiSiBuDeJie/BaiSiBuDeJie/Classes/FriendTrend/View/QQLoginField.m
@@ -0,0 +1,19 @@
+//
+//  QQLoginField.m
+//  BaiSiBuDeJie
+//
+//  Created by 王红庆 on 2018/6/16.
+//  Copyright © 2018年 Mac. All rights reserved.
+//
+
+#import "QQLoginField.h"
+
+@implementation QQLoginField
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+    self.tintColor = [UIColor whiteColor];
+}
+
+@end
