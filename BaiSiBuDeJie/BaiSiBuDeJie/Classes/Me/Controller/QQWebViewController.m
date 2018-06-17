19cc7bfa8c9d13ed0dca64e6885d719d2e3c2451
hongqingWang <272338444@qq.com>
2018-06-17 16:25:41 +0800

    利用KVO设置WKWebView 的属性
    利用KVO设置WKWebView 的属性

--- BaiSiBuDeJie/BaiSiBuDeJie/Classes/Me/Controller/QQWebViewController.m
+++ BaiSiBuDeJie/BaiSiBuDeJie/Classes/Me/Controller/QQWebViewController.m
@@ -14,11 +14,15 @@
 @property (weak, nonatomic) IBOutlet UIView *contentView;
 /// WebView
 @property (nonatomic, weak) WKWebView *webView;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *backItem;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *forwordItem;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *reloadItem;
 
 @end
 
 @implementation QQWebViewController
 
+#pragma mark - Life Cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     
@@ -28,12 +32,44 @@
     
     NSURLRequest *request = [NSURLRequest requestWithURL:_url];
     [webView loadRequest:request];
+    
+    [webView addObserver:self forKeyPath:@"canGoBack" options:NSKeyValueObservingOptionNew context:nil];
+    [webView addObserver:self forKeyPath:@"canGoForward" options:NSKeyValueObservingOptionNew context:nil];
+    [webView addObserver:self forKeyPath:@"title" options:NSKeyValueObservingOptionNew context:nil];
 }
 
 - (void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
-    NSLog(@"%@", NSStringFromCGRect(self.contentView.bounds));
+    
     _webView.frame = self.contentView.bounds;
+}
+
+- (void)dealloc {
+    
+    [self.webView removeObserver:self forKeyPath:@"canGoBack"];
+    [self.webView removeObserver:self forKeyPath:@"canGoForward"];
+    [self.webView removeObserver:self forKeyPath:@"title"];
+}
+
+#pragma mark - Observer
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    
+    self.backItem.enabled = self.webView.canGoBack;
+    self.forwordItem.enabled = self.webView.canGoForward;
+    self.title = self.webView.title;
+}
+
+#pragma mark - Event Response
+- (IBAction)back:(UIBarButtonItem *)sender {
+    [self.webView goBack];
+}
+
+- (IBAction)forword:(UIBarButtonItem *)sender {
+    [self.webView goForward];
+}
+
+- (IBAction)reload:(UIBarButtonItem *)sender {
+    [self.webView reload];
 }
 
 @end
