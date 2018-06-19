ac0b553e07eede679d8113a4de5b59bd1bcff0c6
hongqingWang <272338444@qq.com>
2018-06-19 19:35:06 +0800

    解决数据量小时上拉和下拉刷新的 Bug
    解决数据量小时上拉和下拉刷新的 Bug

--- BaiSiBuDeJie/BaiSiBuDeJie/Classes/Essence/Controller/QQAllViewController.m
+++ BaiSiBuDeJie/BaiSiBuDeJie/Classes/Essence/Controller/QQAllViewController.m
@@ -10,6 +10,10 @@
 
 @interface QQAllViewController ()
 
+@property (nonatomic, weak) UIView *header;
+@property (nonatomic, weak) UILabel *headerLabel;
+@property (nonatomic, assign, getter=isHeaderRefreshing) BOOL headerRefreshing;
+
 @property (nonatomic, weak) UIView *footerView;
 @property (nonatomic, weak) UILabel *footerLabel;
 @property (nonatomic, assign, getter=isFooterRefreshing) BOOL footerRefreshing;
@@ -23,7 +27,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.dataCount = 10;
+    self.dataCount = 3;
     
 //    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
 //
@@ -51,6 +55,28 @@
 }
 
 - (void)setupRefresh {
+    
+    UIView *header = [[UIView alloc] init];
+    header.backgroundColor = [UIColor redColor];
+    header.frame = CGRectMake(0, -50, self.tableView.qq_w, 50);
+    [self.tableView addSubview:header];
+    self.header = header;
+    
+    UILabel *headerLabel = [[UILabel alloc] init];
+    headerLabel.text = @"下拉可以刷新";
+    headerLabel.textColor = [UIColor whiteColor];
+    headerLabel.textAlignment = NSTextAlignmentCenter;
+    headerLabel.frame = header.bounds;
+    [header addSubview:headerLabel];
+    self.headerLabel = headerLabel;
+    
+    UILabel *adLabel = [[UILabel alloc] init];
+    adLabel.backgroundColor = [UIColor blackColor];
+    adLabel.text = @"广告";
+    adLabel.textColor = [UIColor whiteColor];
+    adLabel.textAlignment = NSTextAlignmentCenter;
+    adLabel.frame = CGRectMake(0, 0, self.tableView.qq_w, 30);
+    self.tableView.tableHeaderView = adLabel;
     
     UIView *footerView = [[UIView alloc] init];
     footerView.backgroundColor = [UIColor redColor];
@@ -106,7 +132,67 @@
 }
 
 #pragma mark - TableViewDelegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 44;
+}
+
+#pragma mark - UIScrollViewDelegate
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    
+    [self dealHeader];
+    [self dealFooter];
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+    
+    if (self.isHeaderRefreshing) return;
+    
+    CGFloat offsetY = - (self.tableView.contentInset.top + QQNavigationMaxY + self.header.qq_h);
+    
+    if (self.tableView.contentOffset.y <= offsetY) {
+        
+        self.headerLabel.text = @"正在刷新数据...";
+        self.header.backgroundColor = [UIColor blueColor];
+        self.headerRefreshing = YES;
+        
+        [UIView animateWithDuration:0.25 animations:^{
+            UIEdgeInsets inset = self.tableView.contentInset;
+            inset.top += self.header.qq_h;
+            self.tableView.contentInset = inset;
+        }];
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            
+            self.dataCount = 20;
+            [self.tableView reloadData];
+
+            self.headerRefreshing = NO;
+            
+            [UIView animateWithDuration:0.25 animations:^{
+                UIEdgeInsets inset = self.tableView.contentInset;
+                inset.top -= self.header.qq_h;
+                self.tableView.contentInset = inset;
+            }];
+        });
+    }
+}
+
+- (void)dealHeader {
+    
+    if (self.isHeaderRefreshing) return;
+    
+    CGFloat offsetY = - (self.tableView.contentInset.top + QQNavigationMaxY + self.header.qq_h);
+    
+    if (self.tableView.contentOffset.y <= offsetY) {
+        self.header.backgroundColor = [UIColor lightGrayColor];
+        self.headerLabel.text = @"松开立即刷新";
+    } else {
+        self.header.backgroundColor = [UIColor redColor];
+        self.headerLabel.text = @"下拉可以刷新";
+    }
+}
+
+- (void)dealFooter {
     
     if (self.tableView.contentSize.height == 0) return;
     
@@ -114,26 +200,22 @@
     
     if (self.isFooterRefreshing) return;
     
-    if (self.tableView.contentOffset.y >= offsetY) {
+    if (self.tableView.contentOffset.y >= offsetY && self.tableView.contentOffset.y > -(self.tableView.contentInset.top + QQNavigationMaxY)) {
         
         self.footerRefreshing = YES;
         self.footerView.backgroundColor = [UIColor blueColor];
         self.footerLabel.text = @"正在加载更多数据...";
         
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-
+            
             self.dataCount += 5;
             [self.tableView reloadData];
-
+            
             self.footerRefreshing = NO;
             self.footerView.backgroundColor = [UIColor redColor];
             self.footerLabel.text = @"上拉加载更多";
         });
     }
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return 44;
 }
 
 @end
